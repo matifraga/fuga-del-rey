a2b5cc42e55ba43224d0e9023b91499fee0655a9
mcavo <mvictoria.cavo@gmail.com>
2014-11-02 14:11:20 -0300

    Test

--- src/main/Test.java
+++ src/main/Test.java
@@ -5,23 +5,30 @@ import java.io.File;
 import game.Game;
 
 public class Test {
-
+	
+	/** 
+	 * Heuristica: Ir a Game, metodo value y modificar el metodo llamado alli por value1, value2 o value3
+	 * Poda por simetrias: Ir a Minimax y cambiar los nombres de los metodos minimax por minimax2 y viseversa
+	 */
+	
 	public static void main(String[] args) {
 		//EVALUACIONES DE PROFUNDIDAD
 		Game game;
 		String file;
-		int depth;
-		long time;
-		System.out.println("*************************");
-		System.out.println("*************************");
-		System.out.println("       Sin podas");
-		System.out.println("*************************");
-		System.out.println("*************************");
+		//int depth;
+		//long time;
+		//int acum;
+		System.out.println("//////////////////////////");
+		System.out.println("//////////////////////////");
+		System.out.println("////////Sin podas/////////");
+		System.out.println("//////////////////////////");
+		System.out.println("//////////////////////////");
 		System.out.println();
 		for(int i=7;i<20;i+=4) {
 			System.out.println("*************************");
 			System.out.println("Tablero de dimension "+i);
 			System.out.println("*************************");
+			//acum=0;
 			for(int j=1;j<16;j++) {
 				game= new Game();
 				file="tablerosEvaluaciones/"+i+"/tablero"+j+".txt";
@@ -31,14 +38,24 @@ public class Test {
 					System.out.println(e.getMessage());
 					return;
 				}
-				depth=(i<12)?3:2;
-				time = 0;
-				game.setParameters(0, depth, false);
-				time=System.currentTimeMillis();
+				/*if(i==19) {
+					depth=2;
+				} else {
+					depth=(i==7)?4:3;
+				}*/
+				//time = 0;
+				game.setParameters(10, 0, false);
+				//time=System.currentTimeMillis();
+				System.out.println("------------------------");
+				System.out.println("        Tablero"+j);
+				System.out.println("------------------------");
 				game.getBestNextMove(false);
-				time=System.currentTimeMillis()-time;
-				System.out.println("El Tablero"+j+" tardo: "+time);
+				//time=System.currentTimeMillis()-time;
+				//acum+=time;
+				
 			}
+			//acum/=15;
+			//System.out.println("PROMEDIO = "+acum);
 		}
 	}
 }
